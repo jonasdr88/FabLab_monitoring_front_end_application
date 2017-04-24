395948696a8f26329769422bb45dda5c67440e7f
Jonas <derynck.games@gmail.com>
2017-04-24 13:26:54 +0200

    label array - errors

--- src/FabLab/View/SelectionOverviewController.java
+++ src/FabLab/View/SelectionOverviewController.java
@@ -6,15 +6,19 @@ import FabLab.Model.Material;
 import FabLab.Model.User;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Map;
 
 /**
  * Created by jonas on 11/04/2017.
  */
 public class SelectionOverviewController {
 
+    @FXML
+    private Button startButton;
     @FXML
     private ComboBox<String> machineBox;
     @FXML
@@ -34,20 +38,78 @@ public class SelectionOverviewController {
     @FXML
     private Label selectedMachineLabel;
     @FXML
-    private Label selectedMaterialLabel;
+    private Label selectedMaterialLabel1;
+    @FXML
+    private Label selectedMaterialLabel2;
+    @FXML
+    private Label selectedMaterialLabel3;
+    @FXML
+    private Label selectedMaterialLabel4;
+    @FXML
+    private Label selectedMaterialLabel5;
+    @FXML
+    private TextField materialQuantityTF1;
+    @FXML
+    private TextField materialQuantityTF2;
+    @FXML
+    private TextField materialQuantityTF3;
+    @FXML
+    private TextField materialQuantityTF4;
+    @FXML
+    private TextField materialQuantityTF5;
+    @FXML
+    private Button plusButton;
 
     // reference to main application
     private MainApp mainapp;
     private Machine selectedMachine;
-    private Material selectedMaterial;
+    private Map<Material, String> selectedMaterials;
+    private Label[] materialLabels;
+    private TextField[] materialQuantityTFS;
+    private int materialamount;
 
     //constructor is called BEFORE the initialize method
     public SelectionOverviewController()
     {
+        materialLabels = new Label[5];
+        materialQuantityTFS = new TextField[5];
+        materialLabels[0] = selectedMaterialLabel1;
+        materialLabels[1] = selectedMaterialLabel2;
+        materialLabels[2] = selectedMaterialLabel3;
+        materialLabels[3] = selectedMaterialLabel4;
+        materialLabels[4] = selectedMaterialLabel5;
+        materialQuantityTFS[0] = materialQuantityTF1;
+        materialQuantityTFS[1] = materialQuantityTF2;
+        materialQuantityTFS[2] = materialQuantityTF3;
+        materialQuantityTFS[3] = materialQuantityTF4;
+        materialQuantityTFS[4] = materialQuantityTF5;
+
+        materialamount = 0;
+        materialLabels[0].setVisible(false);
+        materialLabels[1].setVisible(false);
+        materialLabels[2].setVisible(false);
+        materialLabels[3].setVisible(false);
+        materialLabels[4].setVisible(false);
+        materialQuantityTFS[0].setVisible(false);
+        materialQuantityTFS[1].setVisible(false);
+        materialQuantityTFS[2].setVisible(false);
+        materialQuantityTFS[3].setVisible(false);
+        materialQuantityTFS[4].setVisible(false);
+
+
+
     }
 
     public void initialize()
     {
+//        materialamount = 0;
+//        for(int i = 0; i<5; i++)
+//        {
+//            materialLabels[i].setVisible(false);
+//            materialLabels[i].setText("");
+//            materialQuantityTFS[i].setVisible(false);
+//            materialQuantityTFS[i].setText("");
+//        }
     }
 
     @FXML
@@ -68,12 +130,29 @@ public class SelectionOverviewController {
         selectedMachine = mainapp.getMachineHashMap().get(machineBox.getValue());
         selectedMachineLabel.setText(machineBox.getValue());
         materialBox.setItems(selectedMachine.getMaterialStringList());
+
+        materialamount = 0;
+//        for(int i = 0; i<5; i++)
+//        {
+//            materialLabels[2].setVisible(false);
+//            materialLabels[2].setText("");
+//            materialQuantityTFS[2].setVisible(false);
+            materialQuantityTFS[2].setText("");
+//        }
+
     }
 
     public void materialSelectHandle()
     {
-        selectedMaterial = selectedMachine.getMaterialHashMap().get(materialBox.getValue());
-        selectedMaterialLabel.setText(materialBox.getValue());
+    }
+
+    public void plusButtonHandle()
+    {
+//        materialLabels[materialamount].setVisible(true);
+//        materialQuantityTFS[materialamount].setVisible(true);
+//        materialLabels[materialamount].setText(materialBox.getValue());
+//        selectedMaterials.put(selectedMachine.getMaterialHashMap().get(materialBox.getValue()), materialQuantityTFS[materialamount].getText());
+//        materialamount++;
     }
 
     public void setUserIDLabel(String UID)
